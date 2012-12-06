181ea28a865819d4fe8e484d74526dc654ab3fa9
ignacio <rozada@gmail.com>
2012-12-05 17:02:38 -0800

    changed READ/WRITE to fprintf and fscanf as per the Windows code, some stuff was commented out

--- LinGrowSim-Oct12/rdgrowth/dump.c
+++ LinGrowSim-Oct12/rdgrowth/dump.c
@@ -14,16 +14,15 @@
 
 extern char triangulation_filename[80];
 
-#define WRITE(name, file) fwrite(&(name), sizeof(name), 1, f)
+// #define WRITE(name, file) fwrite(&(name), sizeof(name), 1, f)
 
 #define dumpid(NODE) (* ( (int *)(& (NODE)->previous_X)))
 
 /* int dumpidcount = 0; */
 void dump()
-BEGIN
+{
 	FILE *f;
 	int nncount; /* to count the number of nodes */
-/*        int concfixedcount; */
 	int length, i;
 	char filename[L_strings];
 	node_list *nl;
@@ -41,85 +40,84 @@ BEGIN
 	fprintf(stderr, "SRD_Growth[%d]: Dumping to file '%s'.", pid,filename);
 	f = fopen(filename, "w");
 
-	IF (f == NULL) THEN
+	if (f == NULL) {
 		fprintf(stderr, "... failed\n");
 		return;
-	ELSE
+	}
+	
+	else {
 		fprintf(stderr, "\n");
-	FI
-	WRITE(a, f);	WRITE(initA, f);
-	WRITE(b, f);	WRITE(B, f);
-	WRITE(c, f);
-	WRITE(d, f);
+	}
+	fprintf(f, "%f\n", a);	
+	fprintf(f, "%f\n", initA);
+	fprintf(f, "%f\n", b);
+	fprintf(f, "%f\n", B);
+	fprintf(f, "%f\n", c);
+	fprintf(f, "%f\n", d);
 
-        WRITE(X_diffuse, f); WRITE(Y_diffuse, f); WRITE(A_diffuse, f);
-	WRITE(iterations, f);
-	WRITE(dumpidcount, f);
-	WRITE(delta_t, f);
-        WRITE(max_initial_length_squared, f);
-
-	length = strlen(triangulation_filename);
-	WRITE(length, f);
-	FOR (i=0; i<length; i++) DO
-		WRITE(triangulation_filename[i], f);
-	OD
+        fprintf(f, "%f\n", X_diffuse);
+	fprintf(f, "%f\n", Y_diffuse);
+	fprintf(f, "%f\n", A_diffuse);	
+	fprintf(f, "%d\n", iterations);
+	fprintf(f, "%d\n", dumpidcount);
+	fprintf(f, "%f\n", delta_t);
+	fprintf(f, "%f\n", max_initial_length_squared);
+	
+// 	length = strlen(triangulation_filename);
+// 	fprintf(f, "%d\n", length);
+// 	FOR (i=0; i<length; i++) DO
+// 		WRITE(triangulation_filename[i], f);
+// 	OD
 
 	nl  = first_nl;
 	nncount = 0;
-/*        concfixedcount = 0; */
-	WHILE (nl != NULL) DO
+	while (nl != NULL) {
 		node *n= nl->entry;
 		dumpid(n) = nncount;
 		nncount++;
 /*                if(n->conc_fixed = TRUE) concfixedcount++; */
 		nl = nl -> next;
-	OD
+	}
 
 /* this gives the number of nodes! */
 	number_of_nodes = nncount;
-	WRITE(number_of_nodes, f);
+	fprintf(f, "%d\n", number_of_nodes);
 
 	nl = first_nl;
         swcount = 0;
-	WHILE (nl != NULL) DO
+	while (nl != NULL) {
 		node *n = nl->entry;
 		node_list *nbl;
 		double x, y, z, X_val, Y_val, A_val;
 
-		WRITE(n->degree, f);
-		WRITE(n->fixed, f);
-		WRITE(n->conc_fixed, f);
-                WRITE(n->id, f);
+		fprintf(f, "%d\n", n->degree);
+		fprintf(f, "%d\n", n->fixed);
+		fprintf(f, "%d\n", n->conc_fixed);
+		fprintf(f, "%d\n", n->id);
 		x = n->x; y = n->y; z = n->z;
-/*       printf("id %d; x %f; y %f; z %f\n", n->id, n->x, n->y, n->z); */
-		WRITE(x, f);
-		WRITE(y, f);
-		WRITE(z, f);
+		fprintf(f, "%f\n", x);
+		fprintf(f, "%f\n", y);
+		fprintf(f, "%f\n", z);
                 X_val = n->X; Y_val = n->Y; A_val = n->A;
-		WRITE(X_val, f);
-		WRITE(Y_val, f);
-		WRITE(A_val, f);
+		fprintf(f, "%f\n", X_val);
+		fprintf(f, "%f\n", Y_val);
+		fprintf(f, "%f\n", A_val);
 		nbl = n->nl;
-		WHILE(nbl!=NULL) DO
+		WHILE(nbl!=NULL) {
 			node *nb = nbl->entry;
-			WRITE(dumpid(nb), f);
+			fprintf(f, "%d\n", dumpid(nb));
 			nbl = nbl -> next;
-		OD
+		}
 	        nl = nl->next;
-	OD
-
-	    fclose(f);
-
-/*        if(number_of_nodes == concfixedcount) user_stops_exe = TRUE; */
-
-
-END
+	}
+	fclose(f);
+}
 
 
 #define READ(name, file)  fread(&(name), sizeof(name), 1, f)
 
 void undump(char *filename)
-BEGIN
+{
 	FILE *f;
 	int length, i, neighbour;
         char fullfilename[L_strings];
@@ -129,88 +127,95 @@ BEGIN
         sprintf(fullfilename, "%s/%s",old_result_dir,filename);
 	fprintf(stderr, "Attemtping to open %s", fullfilename);
 	f = fopen(fullfilename, "r");
-	IF (f==NULL) THEN
+	if (f==NULL) {
 		fprintf(stderr, " ... unsuccessful.\n");
 		return;
-	ELSE
+	}
+	else  {
 		fprintf(stderr, " ... ");
-	FI
+	}
 
 	/* NOTE - this overwrites the values in TEST, disable this
                   READ if these parameters need changing between 
                   initial and pick-up runs. 8/21/03. */
   
-	READ(a, f);	READ(initA, f);
-	READ(b, f);	READ(B, f);
-	READ(c, f);
-	READ(d, f);
+	fscanf(f, "%f\n", &a);	
+	fscanf(f, "%f\n", &initA);
+	fscanf(f, "%f\n", &b);
+	fscanf(f, "%f\n", &B);
+	fscanf(f, "%f\n", &c);
+	fscanf(f, "%f\n", &d);
 
-        READ(X_diffuse, f); READ(Y_diffuse, f); READ(A_diffuse, f);
-	READ(iterations, f);
-	READ(dumpidcount, f);
-    	READ(delta_t, f);
-        READ(max_initial_length_squared, f);
+        fscanf(f, "%f\n", &X_diffuse);
+	fscanf(f, "%f\n", &Y_diffuse);
+	fscanf(f, "%f\n", &A_diffuse);
+	fscanf(f, "%d\n", &iterations);
+	fscanf(f, "%d\n", &dumpidcount); 
 
-	READ(length, f);
-	FOR (i=0; i<length; i++) DO
-		READ(triangulation_filename[i], f);
-	OD
-	triangulation_filename[length] = '\0';
+	fscanf(f, "%f\n", &delta_t);
+	fscanf(f, "%f\n", &max_initial_length_squared);
 
-	READ(number_of_nodes, f);
-       printf("undump: number_of_nodes is %d\n",number_of_nodes);
+// 	READ(length, f);
+// 	FOR (i=0; i<length; i++) DO
+// 		READ(triangulation_filename[i], f);
+// 	OD
+// 	triangulation_filename[length] = '\0';
+
+	fscanf(f, "%d\n", &number_of_nodes);
+	printf("undump: number_of_nodes is %d\n",number_of_nodes);
 	initial_node_array = malloc (number_of_nodes * sizeof(struct node_s));
 
-	IF (initial_node_array == NULL) THEN
+	if (initial_node_array == NULL) {
  		error_code = OUT_OF_MEMORY;
 		fprintf(stderr, "\nSRD_Growth: Out of Memory\n");
-	   	EXIT;
-	FI
+	   	exit(1);
+	}
 
 
-	FOR (i=0; i<number_of_nodes; i++) DO
+	for (i=0; i<number_of_nodes; i++) {
 		double x,y,z, X_val, Y_val, A_val;
 		node *n = &(initial_node_array[i]);
 		short j;
 
-		READ(n->degree, f);
-
-/*           printf("degree is %d,", n->degree); */
-		READ(n->fixed, f);
-/*           printf("fixed is %d\n", n->fixed); */
-		READ(n->conc_fixed, f);
-                READ(n->id, f);
-		READ(x,f); READ(y,f); READ(z,f);
+		fscanf(f, "%d\n", &n->degree);
+		fscanf(f, "%d\n", &n->fixed);
+		fscanf(f, "%d\n", &n->conc_fixed);
+		fscanf(f, "%d\n", &n->id);
+		fscanf(f, "%f\n", &x);
+		fscanf(f, "%f\n", &y);
+		fscanf(f, "%f\n", &z);
 		n->x = x; n->y = y; n->z = z;
-                READ(X_val, f); READ(Y_val,f); READ(A_val, f);
+		fscanf(f, "%f\n", &X_val);
+		fscanf(f, "%f\n", &Y_val);
+		fscanf(f, "%f\n", &A_val);
                 n->X = X_val; n->Y = Y_val; n->A = A_val;
 
 		n->nl = new_node_list();
 		running_nl = n->nl;
-		READ(neighbour, f);
+		fscanf(f, "%d\n", &neighbour);
 		running_nl -> entry = &(initial_node_array[neighbour]);
 
-		FOR (j=1; j<n->degree; j++) DO
-			READ(neighbour, f);
+		for (j=1; j<n->degree; j++) {
+			fscanf(f, "%d\n", &neighbour);
 			running_nl -> next = new_node_list();
 			running_nl = running_nl -> next;
 			running_nl -> entry = &(initial_node_array[neighbour]);
 			running_nl -> next = NULL;
-		OD
-	OD
+		}
+	}
 
 	first_nl = new_node_list();
 	running_nl = first_nl;
 	running_nl -> entry = &(initial_node_array[0]);
-	FOR (i=1; i<number_of_nodes; i++) DO
+	for (i=1; i<number_of_nodes; i++) {
 		running_nl -> next = new_node_list();
 		running_nl = running_nl -> next;
 		running_nl -> entry = &(initial_node_array[i]);
-	OD
+	}
 
 	running_nl -> next = NULL;
 	last_nl = running_nl;
 
 	fclose(f);
 	fprintf(stderr, "done.\n");
-END	
+}
